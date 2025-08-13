c40ec5dc62fddf9f7f470962f1fec8b3bb6b3582
Anna Gribova <gribova-rec@yandex.ru>
2025-08-13 16:35:01 +0300

    first commit_1

--- order_burger_data.py
+++
@@ -1,6 +0,0 @@
-
-
-class OrderBurgerData:
-
-    ingredients_data = [("Space Beef", "FILLING", 75.0), ("Star's dash", "FILLING", 20.0), ("Asteroid cheese", "FILLING", 15.0)]
-    expected_price_data = 210.0
